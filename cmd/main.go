b2ef0ba6febd8669b76422300daed960adb5cbf2
fumiakikobayashi <vbxy95xwy@gmail.com>
2023-09-14 16:45:44 +0900

    main.goのリファクタリング

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/sashabaranov/go-openai"
-	"go-echo-rest-api-sample/src"
 	"go-echo-rest-api-sample/src/Infrastructures"
-	"go-echo-rest-api-sample/src/Shared"
-	"go.uber.org/zap"
-	"net/http"
+	"go-echo-rest-api-sample/src/Presentations"
+	"go-echo-rest-api-sample/src/Presentations/Routes"
 	"os"
 )
 
@@ -30,68 +27,24 @@ func main() {
 		}
 	}(db)
 
-	// ログ設定
-	logger := Shared.NewLogger()
-
 	// OpenAIのクライアント
 	client := openai.NewClient(os.Getenv("OPENAI_API_KEY"))
-
-	// 依存性の注入したハンドラーを取得
-	handlers := src.NewHandlers(db, &logger, client)
 
 	// echoの初期化
 	e := echo.New()
 
-	// カスタムエラーハンドラー
-	e.HTTPErrorHandler = customHTTPErrorHandler
+	// 依存性の注入したハンドラーを取得
+	handlers := Presentations.NewHandlers(db, client)
 
-	// Middleware
+	// エンドポイントを定義
+	Routes.SetUpRoutes(e, handlers)
+
+	// ミドルウェアの登録
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-
-	// Routes
-	e.GET("/tasks", handlers.TaskHandler.GetTasks)
-	e.GET("/tasks/:taskId", handlers.TaskHandler.GetTask)
-	e.POST("/tasks", handlers.TaskHandler.SaveTask)
-	e.PUT("/tasks/:taskId", handlers.TaskHandler.UpdateTask)
-	e.DELETE("/tasks/:taskId", handlers.TaskHandler.DeleteTask)
-	e.PATCH("/tasks/:taskId/favorite", handlers.TaskHandler.UpdateTaskFavorite)
-	e.PATCH("/tasks/:taskId/complete", handlers.TaskHandler.UpdateTaskComplete)
-	e.GET("/tasks/suggestion", handlers.SuggestedTaskHandler.GetSuggestedTasks)
 
 	// Start server
 	if err := e.Start(":8080"); err != nil {
 		panic(err.Error())
-	}
-}
-
-func customHTTPErrorHandler(err error, c echo.Context) {
-	he, ok := err.(*echo.HTTPError)
-	if !ok {
-		zap.S().Errorf("Unknown error: %v", err)
-		c.JSON(http.StatusInternalServerError, err)
-		return
-	}
-
-	httpCode := he.Code
-	switch err := he.Message.(type) {
-	case error:
-		switch {
-		case httpCode >= 500:
-			zap.S().Errorf("Server error: %v", err)
-			if me, ok := err.(*Shared.SampleError); ok {
-				fmt.Print(me.StackTrace)
-			}
-		case httpCode >= 400:
-			zap.S().Infof("Clients error: %v", err)
-		}
-		c.JSON(httpCode, "error")
-	case string:
-		// 存在しないエンドポイントが叩かれた場合
-		zap.S().Errorf("Echo HTTP error: %v", he)
-		c.JSON(http.StatusInternalServerError, he)
-	default:
-		zap.S().Errorf("Unknown HTTP error: %v", he)
-		c.JSON(http.StatusInternalServerError, "予期せぬエラーが発生しました")
 	}
 }
